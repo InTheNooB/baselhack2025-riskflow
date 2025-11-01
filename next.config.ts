46cc4077c4f1df4a6e62abcb8d70e185191c2016
Lionel Ding <lionel.ding@hotmail.ch>
2025-11-01 12:49:25 +0100

    Who's in paris ?

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  transpilePackages: ["@prisma/client"],
 };
 
 export default nextConfig;
